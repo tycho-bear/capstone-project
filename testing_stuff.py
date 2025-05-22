b874c9f23719ae1b4221441f911db6aa79830559
Tycho Bear <67806683+tycho-bear@users.noreply.github.com>
2025-05-22 10:50:45 -0700

    Testing city swapping, works great

--- testing_stuff.py
+++ testing_stuff.py
@@ -5,24 +5,43 @@ def test_stuff():
     """"""
     city1 = City("Seattle", 2, 5)
     city2 = City("Boston", 5, 4)
-    city3 = City("Austin", 3, 2)
+    city3 = City("Austin", 3, 3)
+    print()
 
-    # works
+    # two city distance - works
     distance = city1.distance_to(city2)
     print(f"Euclidean distance from {city1.name} to {city2.name}: {distance}")
+    print()
 
-    # works
+    # tour distance - works
     test_tour = Tour([city1, city2, city3])
     tour_distance = test_tour.calculate_tour_distance()
     print(f"Euclidean distance of the entire tour: {tour_distance}")
+    print()
 
-    # works
+    # random city generation - works
     random_cities = generate_random_cities(5, 1, 10, 1, 10)
     random_tour = Tour(random_cities)
     print("Randomly generated cities:")
     for i in range(random_tour.num_cities):
-        print(random_tour.cities[i])
+        print(f"\t{random_tour.cities[i]}")
+    print()
 
+    # swapping two cities - works
+    # swap city1 and city3
+    pos1 = 1
+    shift1 = 2  # 1 + 2 = 3
+    print(f"Starting tour:\t{random_tour}")
+    new_tour = random_tour.swap_cities(pos1, shift1)
+    print(f"After swapping:\t{new_tour}")
+
+    print()
+    pos2 = 3
+    shift2 = 2
+    print(f"New tour:\t\t{new_tour}")
+    new_new_tour = new_tour.swap_cities(pos2, shift2)
+    print(f"After swapping:\t{new_new_tour}")
+    print(f"new_tour:\t\t{new_tour}")
 
 
 def main():
