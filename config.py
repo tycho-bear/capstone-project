d466ca5fed549da9c5559d4307f33121062db120
Tycho Bear <67806683+tycho-bear@users.noreply.github.com>
2025-06-02 16:14:33 -0700

    Added different combinations of hyperparameters to the config file. Can just copy/paste these into the main part of the code to use them.

--- config.py
+++ config.py
@@ -3,3 +3,95 @@ x_min_WA = -125
 x_max_WA = -117
 y_min_WA = 45.5
 y_max_WA = 49
+
+
+# ==========================================================================
+# |  Different combinations of hyperparameters below:
+# ==========================================================================
+
+# Distance 20.839
+# max_iterations = 8000
+# initial_temperature = 40
+# cooling_rate = 0.999
+# num_cities = 20
+# shift_max = 2
+
+# Distance 20.548
+# max_iterations = 8000
+# initial_temperature = 33
+# cooling_rate = 0.999
+# num_cities = 20
+# shift_max = 2
+
+# Distance 20.839
+# max_iterations = 10000
+# initial_temperature = 40
+# cooling_rate = 0.999
+# num_cities = 20
+# shift_max = 2
+
+# Distance 21.315
+# max_iterations = 10000
+# initial_temperature = 46
+# cooling_rate = 0.999
+# num_cities = 20
+# shift_max = 2
+
+# Distance 19.752
+# max_iterations = 10000
+# initial_temperature = 17
+# cooling_rate = 0.999
+# num_cities = 20
+# shift_max = 2
+
+# Distance 19.780
+# max_iterations = 10000
+# initial_temperature = 5
+# cooling_rate = 0.999
+# num_cities = 20
+# shift_max = 2
+
+# Distance 19.782
+# max_iterations = 10000
+# initial_temperature = 2.5
+# cooling_rate = 0.999
+# num_cities = 20
+# shift_max = 2
+
+# ==================
+
+
+# 25 cities, distance 26.243
+# max_iterations = 10000
+# initial_temperature = 9
+# cooling_rate = 0.999
+# num_cities = 20
+# shift_max = 10
+
+# 49 cities, distance 56.055
+# max_iterations = 20000
+# initial_temperature = 12
+# cooling_rate = 0.9995
+# num_cities = 20
+# shift_max = 20
+
+# 64 cities, distance 73.265
+# max_iterations = 30000
+# initial_temperature = 20
+# cooling_rate = 0.9997
+# num_cities = 20
+# shift_max = 30
+
+# # 64 cities, distance 72.028
+# max_iterations = 40000
+# initial_temperature = 19
+# cooling_rate = 0.9997
+# num_cities = 20
+# shift_max = 32
+
+# 64 cities, distance 69.136
+max_iterations = 50000
+initial_temperature = 29
+cooling_rate = 0.9998
+num_cities = 20
+shift_max = 32
