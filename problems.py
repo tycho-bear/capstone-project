8c05664b539a44e9f5622699a41cbd6a89c7d229
Tycho Bear <67806683+tycho-bear@users.noreply.github.com>
2025-06-10 15:45:17 -0700

    Started implementing and testing GA methods.

--- problems.py
+++ problems.py
@@ -11,6 +11,7 @@ from helper_classes import Tour
 from helper_functions import generate_random_cities, generate_square_grid
 import numpy as np
 from config import seed
+import math
 
 
 np.random.seed(seed)
@@ -79,22 +80,116 @@ class TravelingSalesmanProblem(Problem):
     # ==========================================================================
 
     def sort_by_fitness(self, population: list[Tour]) -> list[Tour]:
-        """"""
+        """
+        Simple helper function that sorts a given population according to
+        fitness value. Most fit individuals are at the start of the list. For
+        the TSP, this sorts in ascending order according to the tour distance.
 
-    def get_elite(self, sorted_population: list[Tour]) -> list[Tour]:
-        """"""
+        :param population: (list[Tour]) The population to sort.
+        :return: (list[Tour]) The population, sorted by tour distance in
+            ascending order.
+        """
+
+        new_population = sorted(population, key=lambda tour: tour.tour_distance)
+        return new_population
+
+
+    def get_elite(self, sorted_population: list[Tour],
+                  elitism_percent) -> list[Tour]:
+        """
+        Simple function to extract
+
+        :param sorted_population: (list[Tour]) A population sorted in ascending
+            order by tour distance.
+        :param elitism_percent: (float) The percentage of the population to
+            retain for the next generation. Ceiling will be used if this does
+            not come out to a whole number.
+        :return: (list[Tour]) The elite subset of the population.
+        """
+
+        population_size = len(sorted_population)
+        count_to_retain = math.ceil(elitism_percent * population_size)
+        elite = sorted_population[:count_to_retain]
+        return elite
+
 
     def tournament_selection(self, population: list[Tour], num_samples: int) -> Tour:
-        """"""
+        """
+        Chooses `num_samples` Tours from the population without replacement and
+        returns the one with the shortest tour distance.
 
-    def crossover(self, parent1: Tour, parent2: Tour) -> list[Tour]:
-        """"""
+        :param population: (list[Tour]) The population to choose from.
+        :param num_samples: (int) The number of samples, without replacement.
+        :return: (Tour) The shortest Tour from the samples.
+        """
+
+        # participants = np.random.choice(population, size=num_samples,
+        #                                 replace=False)  # works fine?
+
+        pop_size = len(population)
+        # indices = np.random.randint(low=0, high=pop_size, size=num_samples)
+        indices = np.random.choice(pop_size, size=num_samples, replace=False)
+        participants = [population[i] for i in indices]
+
+        ###
+        # print("participants:")
+        # for tour in participants:
+        #     print(tour, tour.tour_distance)
+        ###
+
+        best = participants[0]
+        for tour in participants:
+            if tour.tour_distance < best.tour_distance:
+                best = tour
+
+        return best
+
+
+    def crossover(self, parent1: Tour, parent2: Tour):
+        """
+        Performs ordered crossover with the two parents. Randomly chooses a
+        slice from parent 1, then fills in the remaining cities from parent 2.
+
+        :param parent1: (Tour) The first parent.
+        :param parent2: (Tour) The second parent.
+        :return: A child containing elements of parent 1 and parent 2.
+        """
+
+        num_cities = parent1.num_cities
+        # pick random point
+        # slice will be 1/3 to 1/2 the tour size
+        # copy that slice from the first parent to the same position in the child
+        # fill the rest of the child in order from the second parent
+
+
+
+
+
+
 
     def mutate_individual(self, individual: Tour) -> Tour:
-        """"""
+        """
+        Applies a random mutation to the specified Tour. The mutated Tour is
+        returned as a new object.
 
-    def apply_random_mutation(self, population: list[Tour], mutation_prob: float) -> list[Tour]:
-        """"""
+        Mutations can include
+
+        :param individual: (Tour) The Tour to mutate.
+        :return: (Tour) The mutated Tour.
+        """
+
+
+
+
+    def apply_mutation(self, population: list[Tour], mutation_prob: float) -> list[Tour]:
+        """
+        Probabilistically applies mutation to the entire population. Sometimes
+        nothing happens, and sometimes several Tours are changed.
+
+        :param population:
+        :param mutation_prob:
+        :return:
+        """
 
 
 
--- testing/testing_ga_stuff.py
+++ testing/testing_ga_stuff.py
@@ -5,8 +5,10 @@
 # Summer 2025
 # ===============================
 
-from config import seed
+from config import seed, x_min_WA, x_max_WA, y_min_WA, y_max_WA
 import numpy as np
+from helper_functions import generate_random_cities
+from problems import TravelingSalesmanProblem
 
 
 np.random.seed(seed)
@@ -15,14 +17,38 @@ np.random.seed(seed)
 def test_stuff():
     """"""
     # first create a random tour of 10 cities
+    pop = []
+    num_cities = 6
+    population_size = 6
+    for i in range(population_size):
+        pop.append(generate_random_cities(num_cities, x_min_WA, x_max_WA,
+                                           y_min_WA, y_max_WA))
+    problem = TravelingSalesmanProblem(num_cities, 32)
+
+    def print_population(pop):
+        print("Population:")
+        for tour in pop:
+            print(f"\t{tour}, distance {tour.tour_distance:.3f}")
+
+    print_population(pop)
+
+    # sorting by fitness (easy) - works
+    print()
+    sorted_pop = problem.sort_by_fitness(pop)
+    print_population(sorted_pop)
 
 
+    # get elite (easy) - works
+    print()
+    elitism_percent = 0.15
+    elite = problem.get_elite(sorted_pop, elitism_percent)
+    print_population(elite)
 
-    # sorting by fitness (easy)
-
-    # get elite (easy)
-
-    # tournament selection (medium)
+    # tournament selection (medium) - works
+    print()
+    num_samples = 3
+    champion = problem.tournament_selection(sorted_pop, num_samples)
+    print(f"best: {champion}, {champion.tour_distance:.3f}")
 
     # crossover (medium)
 
