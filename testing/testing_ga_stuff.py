f2d2b46e36db8df567994fbb70d12431795fdd13
Tycho Bear <67806683+tycho-bear@users.noreply.github.com>
2025-06-10 10:32:46 -0700

    Moved testing files into a directory

---
+++ testing/testing_ga_stuff.py
@@ -0,0 +1,41 @@
+# ===============================
+# Tycho Bear
+# CSCI.788.01C1
+# Capstone Project
+# Summer 2025
+# ===============================
+
+from config import seed
+import numpy as np
+
+
+np.random.seed(seed)
+
+
+def test_stuff():
+    """"""
+    # first create a random tour of 10 cities
+
+
+
+    # sorting by fitness (easy)
+
+    # get elite (easy)
+
+    # tournament selection (medium)
+
+    # crossover (medium)
+
+    # mutate_individual (easy/medium)
+
+    # apply_random_mutation (easy)
+
+
+def main():
+    """"""
+    test_stuff()
+
+
+if __name__ == '__main__':
+    main()
+
